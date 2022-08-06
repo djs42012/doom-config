5a3286474d323c3634058c39d7c05b4903ad7f7d
david <github@djs.gg>
2022-08-07 00:33:49 +0100

    install dirvish and modus-themes

--- packages.el
+++ packages.el
@@ -16,6 +16,9 @@
 
 (package! solo-jazz-theme)
 (package! command-log-mode)
+(package! dirvish)
+(package! modus-themes)
+
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
 ;; `:files' in the `:recipe':
