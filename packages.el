6855e8cae1b24601577ce2688c35b5a327cfa01a
david <github@djs.gg>
2022-12-22 10:40:12 -0700

    enable evil-escape-mode globally

--- packages.el
+++ packages.el
@@ -22,7 +22,7 @@
   :recipe (:host github
            :repo "rougier/nano-theme"))
 (package! lsp-tailwindcss :recipe (:host github :repo "merrickluo/lsp-tailwindcss"))
-
+(package! evil-escape)
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
 ;; `:files' in the `:recipe':
