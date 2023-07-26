17cf356a655d822afe7d0278529f20639e6f2a16
david <github@djs.gg>
2023-07-25 23:27:13 -0400

    optimize web-mode + tree-sitter

--- init.el
+++ init.el
@@ -176,7 +176,7 @@
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       ;; (web +html +css +lsp)               ; the tubes
+       (web +html +css +lsp +tree-sitter)               ; the tubes
        yaml              ; JSON, but readable
        ;;zig               ; C, but simpler
 
