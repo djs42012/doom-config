c0ffd2fc4522d849e16bab7cd8d1f7d7b60649e4
david <github@djs.gg>
2023-07-31 03:25:14 -0400

    add helpers to toggle visual fill and indent bars

--- init.el
+++ init.el
@@ -37,7 +37,7 @@
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        ;; indent-guides     ; highlighted indent columns
-       ;; ligatures         ; ligatures and symbols to make your code pretty again
+       ;;( ligatures +extra )         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        nav-flash         ; blink cursor line after big motions
@@ -194,4 +194,5 @@
        ;; twitter           ; twitter client https://twitter.com/vnought
 
        :config
-       literate (default +bindings +smartparens))
+       literate
+       (default +bindings +smartparens))
